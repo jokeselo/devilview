5675b6dc535796efe39bed8151be97d77edfa333
Joke le Sare <87913488+jokeselo@users.noreply.github.com>
2023-06-26 18:40:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,7 +7,7 @@ class Config:
     ADMIN_IDS = ["Nmfajis"]
     DUMP_CHANNEL_ID = -1001940823180
     MONGO_DB_ACCESS_URI = "mongodb+srv://mhdfajisnm:yG2Eihi03ytZnKRO@blow100.lpj5xox.mongodb.net/?retryWrites=true&w=majority"
-    MONGO_DB_COLLECTION_NAME = "blow100"
+    MONGO_DB_COLLECTION_NAME = "DB-01"
     SMM_PANEL_API_KEY = "58417498e0aff7055e36e63a9d651eaf"
 
     class Defaults:
